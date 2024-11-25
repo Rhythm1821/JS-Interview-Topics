fd25b6de577987a84bb05b68a2cccf85b70ecd21
Rhythm1821 <alamrawat1821@gmail.com>
2024-11-25 17:49:23 +0530

    scopes in different use cases

--- 02_Closures_Scopes/closures.js
+++ 02_Closures_Scopes/closures.js
@@ -40,3 +40,56 @@ Things to Watch Out For
 Memory Leaks: Closures can unintentionally keep variables in memory longer than necessary if they are referenced by long-lived objects.
 Overuse: Excessive closures can lead to confusing code and potential performance issues.
 */
+
+
+
+
+
+// different use cases of scopes 
+
+// 1. Variables declared in the outer function scope
+/*
+When a closure is created, it "remembers" the variables from its outer function's scope. If you access a variable, 
+it will use the value from the outer scope at the time the closure was created.
+*/
+
+function outer() {
+  let count=0
+  return function increment(){
+      count++
+      return count
+  }
+}
+
+const increment = outer()
+console.log(increment())
+console.log(increment())
+
+
+// 2. Global Scope
+// If the closure does not find a variable in its outer function scope, it will search in the global scope.
+
+let globalVar = "I am a global variable";
+
+function outer() {
+  return function inner(){
+    console.log(globalVar);
+  }
+}
+
+const innerFunction = outer();
+innerFunction() // I am a global variable
+
+
+// 3. Variables Shadowed in the Closure
+
+function outer() {
+  let name = "Outer Scope";
+  return function inner(){
+    let name = "Inner Scope"; // Shadows the outer variable
+    console.log(name);
+  }    
+}
+
+const innerFn = outer();
+innerFn() // Inner Scope
