d05168173b5c9b5c7d572af034446186a65f97f3
Rhythm1821 <alamrawat1821@gmail.com>
2024-12-09 23:23:13 +0530

    polyfills

---
+++ 03_Polyfills/polyfills.js
@@ -0,0 +1,11 @@
+// Polyfills
+
+/*
+Polyfills are JavaScript libraries that provide functionality that is not natively available in the current environment or browser. 
+They are used to add new features to older browsers or platforms that do not support them natively.
+
+Why Use Polyfills?
+1. Cross-Browser Compatibility: Polyfills allow you to write code that works the same across different browsers.
+2. Performance Optimization: Polyfills can improve the performance of your code, especially in older browsers.
+3. Accessibility: Polyfills can help make your code more accessible to users with disabilities.
+*/
